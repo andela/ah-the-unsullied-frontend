53f36a497f59d926365716de981bf3732ead434d
kwanj-k <mwangikelvin23@gmail.com>
2019-02-07 17:55:25 +0300

    chore(heroku): deploy to heroku
    - Add heroku config
    - Have the app working on heroku
    
    [Starts #163783001]

--- src/components/home/views/home.js
+++ src/components/home/views/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const HomeView = () => {
-    return (
-      <div>
-        <h1>{"Welcome to Author's Haven"}</h1>
-      </div>
-    );
+  return (
+    <div>
+      <h1>{'Welcome to Author\'s Haven'}</h1>
+    </div>
+  );
 };
 
 export default HomeView;
