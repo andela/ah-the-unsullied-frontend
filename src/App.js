adf1dc5aeef2cfc777aec0d302909f976bc8cf71
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-02-15 16:21:35 +0300

    feat(login): user login (#12)
    - User can login using the ui
    
    [Starts #162949172]

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { updateCount } from './actions/button';
-import './app.scss';
-import Home from './components/home/home'; 
-import Login from './components/login/login';
-import Navigation from './components/common/nav/Nav';
+import { Home } from './components/home';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-
-    this.props.updateCount(this.state.count);
-  };
-
-  render() {
-    return (
+const App = () => (
+  <div>
+    <BrowserRouter>
       <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Add count</button>
-        <Router>
-          <div className="App">
-            <Navigation />
-            <Switch>
-              <Route path="/" component={Home} exact Strict />
-              <Route path="/login" component={Login} exact />
-            </Switch>
-          </div>
-        </Router>
+        <ToastContainer />
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </BrowserRouter>
+  </div>
+);
 
-App.propTypes = {
-  updateCount: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  count: state.count
-});
-export default connect(
-  mapStateToProps,
-  { updateCount }
-)(App);
+export default App;
--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,2 @@
-export const UPDATE_COUNT = 'UPDATE_COUNT';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const GET_ERRORS = 'GET_ERRORS';
